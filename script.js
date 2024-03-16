0fb7a5afd80cff9bdb98a458584b9ba28f345828
sakuta17 <dino19web@gmail.com>
2024-03-16 19:04:43 +0530

    Initital commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 170 + "px";
+  blur.style.top = dets.y - 170 + "px";
+});
+
+gsap.to("#nav", {
+  backgroundColor: "black",
+  height: "110px",
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    // markers: true,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
+  },
+});
+
+gsap.to("#main", {
+  backgroundColor: "black",
+  //   markers: true,
+
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 1,
+  },
+});
